d8c2fb7242d8c758457682440d4e7b7f476ed131
IrinaCodixis <ijumamuradova@codixis.com>
2015-09-02 09:30:14 -0400

    generate CRUD

---
+++ Form/ImagesType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Mipa\ImageBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ImagesType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('image')
+            ->add('name')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Mipa\ImageBundle\Entity\Images'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'mipa_imagebundle_images';
+    }
+}
