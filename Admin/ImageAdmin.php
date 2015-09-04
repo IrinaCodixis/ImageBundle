4a59fdad0f0ee53330fe053bed7c74aba21839ad
IrinaCodixis <ijumamuradova@codixis.com>
2015-09-04 12:42:49 +0300

    add Admin class and controller

--- Admin/ImageAdmin.php
+++ Admin/ImageAdmin.php
@@ -6,9 +6,76 @@ use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Validator\ErrorElement;
 use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+use Mipa\ImageBundle\Entity\Images;
  
 class ImageAdmin extends Admin
 {
+	 // setup the defaut sort column and order
+    protected $datagridValues = array(
+        '_sort_order' => 'name',
+        
+    );
+ 
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('title')
+			->add('name')
+			->add('file', 'file', array('label' => 'Pictures', 'required' => false))
+        ;
+    }
+ 
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('title')
+			->add('name')
+			->add('file', 'file', array('label' => 'Pictures', 'required' => false))
+        ;
+    }
+ 
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('name')
+            ->add('title')
+			->add('file', 'file', array('label' => 'Pictures', 'required' => false))
+            ->add('_action', 'actions', array(
+                'actions' => array(
+                    'view' => array(),
+                    'edit' => array(),
+                    'delete' => array(),
+                )
+            ))
+        ;
+    }
+ 
+    protected function configureShowField(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('title')
+			->add('name')
+			->add('file', 'file', array('label' => 'Pictures', 'required' => false))
+        ;
+    }
+	
+	public function getBatchActions()
+	{
+		// retrieve the default (currently only the delete action) actions
+		$actions = parent::getBatchActions();
+	 
+		// check user permissions
+		if($this->hasRoute('edit') && $this->isGranted('EDIT') && $this->hasRoute('delete') && $this->isGranted('DELETE')) {
+			$actions['extend'] = array(
+				'label'            => 'Extend',
+				'ask_confirmation' => true // If true, a confirmation will be asked before performing the action
+			);
+	 
+		}
+	 
+		return $actions;
+	}
 }
 
 ?>
