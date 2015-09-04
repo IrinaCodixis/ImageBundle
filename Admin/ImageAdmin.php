8ad44281fcc4f4c71ee6e7de3b7da74b77bb6b14
IrinaCodixis <ijumamuradova@codixis.com>
2015-09-04 11:41:48 +0300

    add image group to services.yml for admin part

---
+++ Admin/ImageAdmin.php
@@ -0,0 +1,14 @@
+<?php
+namespace Mipa\ImageBundle\Admin;
+ 
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Validator\ErrorElement;
+use Sonata\AdminBundle\Form\FormMapper;
+ 
+class ImageAdmin extends Admin
+{
+}
+
+?>
---
+++ Controller/AdminController.php
@@ -0,0 +1,11 @@
+<?php
+namespace Mipa\ImageBundle\Controller;
+ 
+use Sonata\AdminBundle\Controller\CRUDController as Controller;
+ 
+class AdminController extends Controller
+{
+ 
+}
+
+?>
