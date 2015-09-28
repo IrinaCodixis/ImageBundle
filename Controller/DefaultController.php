74baceb210f1a874e6a99f4a86ef7290959447db
IrinaCodixis <ijumamuradova@codixis.com>
2015-09-28 12:10:31 +0300

    return everything on its place

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -4,7 +4,7 @@ namespace Mipa\ImageBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Security\Core\SecurityContext;
-use Mipa\ImageBundle\Entity\User;
+use Mipa\ImageBundle\Entity\User
 
 class DefaultController extends Controller
 {
@@ -17,43 +17,20 @@ class DefaultController extends Controller
     {
         $request = $this->getRequest();
         $session = $request->getSession();
+		$remember = $request->get('remember');
 		
-		
-		$em = $this->getDoctrine()->getManager();
-        $repo = $em->getRepository('MipaImageBundle:User');
-		
-		
-		if ($request->getMethod() == 'POST') {
-			
-			$remember = $request->get('remember');
-			// get the login error if there is one
-			if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
-				$error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
-			} else {
-				if ($remember=='remember-me') {
-				$login = new User();
-				$login->setUsername($username);
-				$login->setPassword($password);
-				$session->set('login', $login);
-				}
-			
-				$error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
-				$session->remove(SecurityContext::AUTHENTICATION_ERROR);
-			}
-		}
-		else {
-			if ($session->has('login')) {
-				$login = $session->get('login');
-				$username = $login->getUsername();
-				$password = $login->getPassword();
-				$user = $repo->findOneBy(array('userName' => $username, '$password' => $password));
-				
-			}
+		// get the login error if there is one
+        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+        } else {
+            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+        }
+ 
         return $this->render('MipaImageBundle:Default:login.html.twig', array(
             // last username entered by the user
             'last_username' => $session->get(SecurityContext::LAST_USERNAME),
-			));
-		}
-	}
-	
+            'error'         => $error,
+        ));
+    }
 }
